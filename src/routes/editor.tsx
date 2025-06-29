18f58937de4c524b1181aca0e26f97cd4d6f1964
Raphael Jorge <raphaeljsn1@gmail.com>
2025-06-29 10:27:02 -0300

    Deleted editor.tsx

--- src/routes/editor.tsx
+++
@@ -1,6 +0,0 @@
-import { EditorPage } from '@/pages/editor'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/editor')({
-  component: EditorPage,
-})
